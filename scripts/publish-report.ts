1e9588e09c07e6542ae33484e373682a065ea379
Ben <ben@ahnlab.io>
2024-11-20 16:32:18 +0900

    feat: error 발생 시 sns publish

--- scripts/publish-report.ts
+++ scripts/publish-report.ts
@@ -1,4 +1,5 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import * as fs from "fs";
 import { readFile } from "node:fs/promises";
 
@@ -8,16 +9,38 @@ import { readFile } from "node:fs/promises";
  * and upload ./playwright-report/index.html file to s3 bucket
  */
 
+interface Suite {
+  title: string;
+  file: string;
+  specs: Spec[];
+}
+
+interface Spec {
+  title: string;
+  ok: boolean;
+}
+
 const bucketName = process.env.BUCKET_NAME;
 const regionName = process.env.REGION_NAME;
+const topicArn = process.env.TOPIC_ARN;
 const bucketWebsiteBaseUrl = `http://${bucketName}.s3-website.ap-northeast-2.amazonaws.com`;
 
 const testReportPublish = async () => {
-  await s3Publish("./playwright-report/index.html", regionName);
-  await snsPublish();
+  const htmlReportUrl = await s3Publish(
+    "./playwright-report/index.html",
+    regionName
+  );
+  const errors = await getErrorSpecs("./playwright-report/test-results.json");
+  if (errors.length > 0) {
+    const message = `Webwallet UI Test Failed! check the report: ${htmlReportUrl}`;
+    await snsPublish(topicArn, regionName, message);
+    return;
+  }
+
+  console.log("All tests passed!");
 };
 
-const s3Publish = async (filePath: string, region: string) => {
+const s3Publish = async (filePath: string, region: string): Promise<string> => {
   const s3 = new S3Client({ region });
   // key 는 오늘 날짜와 시간을 key 로 구분해서 prefix 로 붙여서 저장
   // ex) playwright-reports/2021/09/01/20210901142521-test-report.html
@@ -38,17 +61,38 @@ const s3Publish = async (filePath: string, region: string) => {
 
   const response = await s3.send(command);
   console.log(response);
-  console.log("bucket site url: ", `${bucketWebsiteBaseUrl}/${key}`);
+  console.log(
+    "test report upload. bucket site url: ",
+    `${bucketWebsiteBaseUrl}/${key}`
+  );
+  return `${bucketWebsiteBaseUrl}/${key}`;
 };
 
-const snsPublish = async () => {
-  const data = fs.readFileSync("./playwright-report/test-results.json", "utf8");
+const getErrorSpecs = async (filePath: string): Promise<Spec[]> => {
+  const data = fs.readFileSync(filePath, "utf8");
   const jsonData = JSON.parse(data);
-  if (jsonData.errors.length > 0) {
-    console.log("error.");
-  } else {
-    console.log("no error.");
-  }
+  const specs: Spec[] = jsonData.suites.reduce((acc, suite) => {
+    const specs = suite.specs.map((spec) => spec);
+    return [...acc, ...specs];
+  }, []);
+
+  const errorSpecs = specs.filter((spec) => spec.ok === false);
+  return errorSpecs;
+};
+
+const snsPublish = async (
+  topicArn: string,
+  regionName: string,
+  message: string
+) => {
+  const sns = new SNSClient({ region: regionName });
+  const command = new PublishCommand({
+    TopicArn: topicArn,
+    Message: message,
+  });
+  console.log("sns Published. message: ", message);
+  const response = await sns.send(command);
+  console.log(response);
 };
 
 testReportPublish();
